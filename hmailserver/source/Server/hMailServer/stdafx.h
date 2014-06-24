fc3b833e6baea211cb5b3edbfd6ce1c233adb90f
Martin Knafve <knafve@gmail.com>
2014-06-24 21:08:13 +0200

    Added x64-configuration.

--- hmailserver/source/Server/hMailServer/stdafx.h
+++ hmailserver/source/Server/hMailServer/stdafx.h
@@ -33,21 +33,22 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
-	#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+
+#ifdef _WIN64
+
+   // Windows Server 2003
+   #define WINVER 0x0502	
+   #define _WIN32_WINNT 0x0502	// Change this to the appropriate value to target Windows Server 2008 or later.
+   #define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+
+#else
+
+   #define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+   #define _WIN32_WINNT 0x0500	// Change this to the appropriate value to target Windows 2000 or later.
+   #define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+
 #endif
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows NT 4 or later.
-	#define _WIN32_WINNT 0x0500	// Change this to the appropriate value to target Windows 2000 or later.
-#endif						
-
-#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
-	#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#endif
-
-#ifndef _WIN32_IE			// Allow use of features specific to IE 4.0 or later.
-	#define _WIN32_IE 0x0400	// Change this to the appropriate value to target IE 5.0 or later.
-#endif
 
 // START: ATL settings
 	#define _ATL_FREE_THREADEDLPCWSTR
