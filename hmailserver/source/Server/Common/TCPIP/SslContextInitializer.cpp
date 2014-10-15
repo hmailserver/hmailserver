0d18e89c6828f03b7a712733d5fdca62877f741c
Martin Knafve <knafve@gmail.com>
2014-10-15 17:37:04 +0200

    Revert "Disabled SSLv3 completely. Note that in master branch, a UI option has been added to select which protocols to use."
    Upgrade to OpenSSL 1.0.1j instead.

--- hmailserver/source/Server/Common/TCPIP/SslContextInitializer.cpp
+++ hmailserver/source/Server/Common/TCPIP/SslContextInitializer.cpp
@@ -29,8 +29,7 @@ namespace HM
       try
       {
          context.set_options(boost::asio::ssl::context::default_workarounds |
-            boost::asio::ssl::context::no_sslv2 |
-            boost::asio::ssl::context::no_sslv3);
+            boost::asio::ssl::context::no_sslv2);
       }
       catch (boost::system::system_error ec)
       {
@@ -118,8 +117,7 @@ namespace HM
    { 
       boost::system::error_code errorCode;
       context.set_options(boost::asio::ssl::context::default_workarounds |
-         boost::asio::ssl::context::no_sslv2 |
-         boost::asio::ssl::context::no_sslv3);
+         boost::asio::ssl::context::no_sslv2);
 
       if (errorCode.value() != 0)
       {
