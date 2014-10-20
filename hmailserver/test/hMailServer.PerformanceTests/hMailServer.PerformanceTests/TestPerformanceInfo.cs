77d6656f104b2f8e9107913f23d97ae25156cba6
Martin Knafve <knafve@gmail.com>
2014-10-20 13:15:21 +0200

    Adding performance test tool

---
+++ hmailserver/test/hMailServer.PerformanceTests/hMailServer.PerformanceTests/TestPerformanceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hMailServer.PerformanceTests
+{
+   public static class TestPerformanceInfo
+   {
+      public static Dictionary<string, TimeSpan> Timings = new Dictionary<string, TimeSpan>(); 
+   }
+}
