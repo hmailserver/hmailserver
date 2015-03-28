caa728cc81e8f8c12385bc0241acf4fdd4ba111f
Martin Knafve <knafve@gmail.com>
2015-03-28 20:21:08 +0100

    Database upgrade for 5.7.

--- hmailserver/source/Tools/DBUpdater/formMain.cs
+++ hmailserver/source/Tools/DBUpdater/formMain.cs
@@ -162,6 +162,7 @@ namespace DBUpdater
          _upgradeScripts.Add(new UpgradeScript(5501, 5502));
          _upgradeScripts.Add(new UpgradeScript(5502, 5600));
          _upgradeScripts.Add(new UpgradeScript(5600, 5601));
+         _upgradeScripts.Add(new UpgradeScript(5601, 5700));
       }
 
       private void buttonClose_Click(object sender, EventArgs e)
@@ -265,6 +266,8 @@ namespace DBUpdater
                return "hMailServer 5.6 (Alpha 1)";
             case 5601:
                return "hMailServer 5.6";
+            case 5700:
+               return "hMailServer 5.7";
             default:
                return "Unknown version";
          }
