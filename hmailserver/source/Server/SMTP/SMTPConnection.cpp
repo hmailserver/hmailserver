1cb67437dc3a93b6677932a6a93e5e2b186f09a7
Martin Knafve <knafve@gmail.com>
2015-04-07 21:45:28 +0200

    Improved error message if client tries to authenticate despite authentication being disabled.

--- hmailserver/source/Server/SMTP/SMTPConnection.cpp
+++ hmailserver/source/Server/SMTP/SMTPConnection.cpp
@@ -1647,7 +1647,7 @@ namespace HM
    {
       if (!GetAuthIsEnabled_())
       {
-         SendErrorResponse_(504, "Authentication not allowed.");
+         SendErrorResponse_(504, "Authentication not enabled.");
          return;
       }
 
