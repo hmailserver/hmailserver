f745939e2975e1a7fb3cf177ab89d360ca6eb86a
Martin Knafve <knafve@gmail.com>
2016-04-24 14:52:09 +0200

    Wildcard characters mangled when upgrading database from v5.3 to v5.4. #123

--- hmailserver/source/DBScripts/Upgrade5320to5400MySQL.sql
+++ hmailserver/source/DBScripts/Upgrade5320to5400MySQL.sql
@@ -1,15 +1,5 @@
 insert into hm_settings (settingname, settingstring, settinginteger) values ('EnableWhitelisting', '', 1);
 
-update hm_whitelist set whiteemailaddress = replace(whiteemailaddress, '/', '//');
-
-update hm_whitelist set whiteemailaddress = replace(whiteemailaddress, '%', '/%');
-
-update hm_whitelist set whiteemailaddress = replace(whiteemailaddress, '_', '/_');
-
-update hm_whitelist set whiteemailaddress = replace(whiteemailaddress, '?', '_');
-
-update hm_whitelist set whiteemailaddress = replace(whiteemailaddress, '*', '%');
-
 update hm_greylisting_whiteaddresses set whiteipaddress = replace(whiteipaddress, '/', '//');
 
 update hm_greylisting_whiteaddresses set whiteipaddress = replace(whiteipaddress, '%', '/%');
