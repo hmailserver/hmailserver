9ec9e2c4e5e1391ce7d9069e3df3e70d91b99482
Martin Knafve <knafve@gmail.com>
2019-08-16 15:43:37 +0200

    Fixed incorrect indentation after merge of Return-Path header in SA-integration. #116

--- hmailserver/source/Server/Common/AntiSpam/SpamTestSpamAssassin.cpp
+++ hmailserver/source/Server/Common/AntiSpam/SpamTestSpamAssassin.cpp
@@ -57,16 +57,17 @@ namespace HM
       std::shared_ptr<Message> pMessage = pTestData->GetMessageData()->GetMessage();
       const String sFilename = PersistentMessage::GetFileName(pMessage);
 
-	  // Add Return-Path header if none exist (ExternalAccount download?)
-	  if (pTestData->GetMessageData()->GetReturnPath().IsEmpty())
-	  {
-		  String sEnvelopeFrom = pTestData->GetEnvelopeFrom();
+      // Add Return-Path header if none exist (ExternalAccount download?)
+      if (pTestData->GetMessageData()->GetReturnPath().IsEmpty())
+      {
+         String sEnvelopeFrom = pTestData->GetEnvelopeFrom();
 
-		  std::vector<std::pair<AnsiString, AnsiString> > fieldsToWrite;
-		  fieldsToWrite.push_back(std::make_pair("Return-Path", sEnvelopeFrom));
-		  TraceHeaderWriter writer;
-		  writer.Write(sFilename, pMessage, fieldsToWrite);
-	  }
+         std::vector<std::pair<AnsiString, AnsiString>> fieldsToWrite;
+         fieldsToWrite.push_back(std::make_pair("Return-Path", sEnvelopeFrom));
+         
+         TraceHeaderWriter writer;
+         writer.Write(sFilename, pMessage, fieldsToWrite);
+      }
       
       std::shared_ptr<IOService> pIOService = Application::Instance()->GetIOService();
 
@@ -116,10 +117,12 @@ namespace HM
 
       // Check if the message is tagged as spam.
       std::shared_ptr<MessageData> pMessageData = pTestData->GetMessageData();
-	  pMessageData->RefreshFromMessage();
-	  // Remove Return-Path header.
-	  pMessageData->DeleteField("Return-Path");
-	  pMessageData->Write(sFilename);
+
+      pMessageData->RefreshFromMessage();
+      // The Return-Path header was added above to help SpamAssassin with its SPF checks.
+      // We should remove it again to restore the headers to original state (except for any added by SA).
+      pMessageData->DeleteField("Return-Path");
+      pMessageData->Write(sFilename);
 
       bool bIsSpam = false;
       AnsiString sSpamStatus = pMessageData->GetFieldValue("X-Spam-Status");
