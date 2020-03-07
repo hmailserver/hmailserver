6f5e22c26463cca2457d2041cb3ed87d7aec5a7c
RvdHout <ruud.vd.hout@hotmail.com>
2020-03-07 13:43:26 +0100

    Update SURBL.cpp
    Better URL regex for SURBL?
    
    For more info, checkout this topic: https://www.hmailserver.com/forum/viewtopic.php?f=7&t=34743

--- hmailserver/source/Server/Common/AntiSpam/SURBL.cpp
+++ hmailserver/source/Server/Common/AntiSpam/SURBL.cpp
@@ -38,9 +38,9 @@ namespace HM
       String sBody = pMessageData->GetBody() + pMessageData->GetHTMLBody(); 
 
       // Extract URL's from the mail body:
-      // Original: https?:\/\/([^?><\\ \"'\/]*)
+      // Original: (?:(?>https?)?(?>:\/\/|\%3A\%2F\%2F))(?:www\.)?([a-z0-9\-\.\=\r\n]+)
 
-      String sRegex = "https?:\\/\\/([^?><\\\\ \\\"'\\/]*)";
+      String sRegex = "(?:(?>https?)?(?>:\\/\\/|\\%3A\\%2F\\%2F))(?:www\\.)?([a-z0-9\\-\\=\\.\\r\\n]+)";
 
       std::set<String> addresses;
 
@@ -48,7 +48,7 @@ namespace HM
 
       try
       {
-         boost::wregex expression(sRegex);
+         boost::wregex expression(sRegex, boost::wregex::icase);
          boost::wsmatch matches;
 
          String sRemainingSearchSpace = sBody;
