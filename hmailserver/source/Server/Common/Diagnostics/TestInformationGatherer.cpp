172ea2c31c15bf7c9459a7c94f7a7d517ff5888f
Martin Knafve <knafve@gmail.com>
2022-01-15 17:17:39 +0100

    Removed meaningless/incorrect info from diagnostics info. #397

--- hmailserver/source/Server/Common/Diagnostics/TestInformationGatherer.cpp
+++ hmailserver/source/Server/Common/Diagnostics/TestInformationGatherer.cpp
@@ -34,29 +34,8 @@ namespace HM
       formattedLine.Format(_T("hMailServer version: %s\r\n"), Application::Instance()->GetVersionNumber().c_str());
       result.append(formattedLine);
 
-      formattedLine.Format(_T("Version architecture: %s\r\n"), Application::Instance()->GetVersionArchitecture().c_str());
-      result.append(formattedLine);
-
       SYSTEM_INFO system_info;
       GetSystemInfo(&system_info);
-
-      String processor_architecture;
-
-      switch (system_info.wProcessorArchitecture)
-      {
-      case PROCESSOR_ARCHITECTURE_INTEL:
-      case PROCESSOR_ARCHITECTURE_AMD64:
-         processor_architecture = "x86";
-         break;
-      case PROCESSOR_ARCHITECTURE_IA64:
-         processor_architecture = "x64";
-         break;
-      default:
-         processor_architecture = "Unknown";
-      }
-
-      formattedLine = Formatter::Format("Processor architecture: {0}\r\n", processor_architecture);
-      result.append(formattedLine);
 
       formattedLine = Formatter::Format("Logical processors: {0}\r\n", (int) system_info.dwNumberOfProcessors);
       result.append(formattedLine);
