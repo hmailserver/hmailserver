199673af4fa2c3142a95d34b0273dfefd6528c31
RvdH <ruud.vd.hout@hotmail.com>
2022-01-24 14:21:28 +0100

    fix upgrade scripts
    fix upgrade scripts

--- hmailserver/source/DBScripts/Upgrade5700to5701MSSQLCE.sql
+++ hmailserver/source/DBScripts/Upgrade5700to5701MSSQLCE.sql
@@ -1,11 +1,3 @@
-insert into hm_settings (settingname, settingstring, settinginteger) values ('ImapMasterUser', '', 0)
-
-insert into hm_settings (settingname, settingstring, settinginteger) values ('ImapAuthAllowPlainText', '', 0)
-
-insert into hm_settings (settingname, settingstring, settinginteger) values ('EnableImapSASLPlain', '', 0)
-
-insert into hm_settings (settingname, settingstring, settinginteger) values ('EnableImapSASLInitialResponse', '', 0)
-
 insert into hm_settings (settingname, settingstring, settinginteger) values ('IPv6Preferred', '', 0)
 
 update hm_dbversion set value = 5701
