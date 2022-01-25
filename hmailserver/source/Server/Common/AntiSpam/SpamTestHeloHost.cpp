c7f64d464f59bbffe795fc4c30118684ae79cc76
RvdH <ruud.vd.hout@hotmail.com>
2022-01-25 21:44:01 +0100

    Update SpamTestHeloHost.cpp
    IPv6 fix for HELO check
    
    Valid HELOs are according to RFC2821 4.1.1.1:
    
    some.domain.name
    [<ipv4-address>]
    [IPv6:<ipv6-address>]

--- hmailserver/source/Server/Common/AntiSpam/SpamTestHeloHost.cpp
+++ hmailserver/source/Server/Common/AntiSpam/SpamTestHeloHost.cpp
@@ -13,6 +13,7 @@
 #include "../TCPIP/DNSResolver.h"
 #include "../TCPIP/IPAddress.h"
 #include "../TCPIP/LocalIPAddresses.h"
+#include <boost/algorithm/string/predicate.hpp>
 
 #ifdef _DEBUG
 #define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
@@ -60,7 +61,7 @@ namespace HM
       {
          // Incorrect host in helo
          String sMessage = "The host name specified in HELO does not match IP address.";
-         int iScore = Configuration::Instance()->GetAntiSpamConfiguration().GetCheckHostInHeloScore();;
+         int iScore = Configuration::Instance()->GetAntiSpamConfiguration().GetCheckHostInHeloScore();
 
          std::shared_ptr<SpamTestResult> pResult = std::shared_ptr<SpamTestResult>(new SpamTestResult(GetName(), SpamTestResult::Fail, iScore, sMessage));
          setSpamTestResults.insert(pResult);   
@@ -76,15 +77,17 @@ namespace HM
    {
       String sIPAddress = address.ToString();
 
-      bool bMatch = false;
-
       if (sHeloHost.Left(1) == _T("["))
       {
          String sTempHost = sHeloHost;
          sTempHost.TrimLeft(_T("["));
+         // IPv6 
+         if (sTempHost.Left(5) == _T("IPv6:"))
+            sTempHost.TrimLeft(_T("IPv6:"));
          sTempHost.TrimRight(_T("]"));
 
-         if (sTempHost == sIPAddress)
+         // IPv6 is alphanumeric therefore uppercase and lowercase characters are equivalent
+         if (boost::iequals(sTempHost, sIPAddress))
          {
             return true;
          }
