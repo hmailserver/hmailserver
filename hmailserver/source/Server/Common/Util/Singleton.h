56e58e1e98ea6ae60f5c38c5eaae6945e51554fd
xth <xth@live.com>
2022-10-18 23:29:06 +0800

    Better singleton implementation The vcruntime 's static initialization is thread-safe. See "...\crt\src\vcruntime\thread_safe_statics.cpp".

--- hmailserver/source/Server/Common/Util/Singleton.h
+++ hmailserver/source/Server/Common/Util/Singleton.h
@@ -8,31 +8,9 @@ class Singleton
 {
 public:
 
-   virtual ~Singleton<T>()
-   {
-
-   }
-
    static T * Instance()
    {
-      if (instance_ == nullptr)
-      {
-         boost::lock_guard<boost::recursive_mutex> guard(create_instance_mutex_);
-
-         instance_ = new T();
-      }
-
-      
-
-      return instance_;
+      static T t;
+      return &t;
    }
-
-private:
-
-   static T * instance_;
-   static boost::recursive_mutex create_instance_mutex_;
-
 };
-
-template <class T> T *Singleton<T>::instance_ = nullptr;
-template <class T> boost::recursive_mutex Singleton<T>::create_instance_mutex_;
