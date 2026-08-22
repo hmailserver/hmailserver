6b4370ca7db01134b9e7d445c242ae46cc2179f7
Martin Knafve <knafve@gmail.com>
2026-08-22 17:26:24 +0200

    Set values to 0 - the default values will be used.

--- hmailserver/source/DBScripts/CreateTablesPGSQL.sql
+++ hmailserver/source/DBScripts/CreateTablesPGSQL.sql
@@ -799,9 +799,10 @@ insert into hm_settings (settingname, settingstring, settinginteger) values ('Cr
 
 insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashAlgorithm', '', 1);
 
-insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashMemoryCost', '', 19456);
+-- 0 means "use algorithm default" (Argon2id: memory cost 19456 KB, 2 iterations).
+insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashMemoryCost', '', 0);
 
-insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashIterations', '', 2);
+insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashIterations', '', 0);
 
 insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashAutoUpgrade', '', 1);
 
--- hmailserver/source/DBScripts/Upgrade5709to5710MySQL.sql
+++ hmailserver/source/DBScripts/Upgrade5709to5710MySQL.sql
@@ -1,8 +1,9 @@
 insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashAlgorithm', '', 1);
 
-insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashMemoryCost', '', 19456);
+-- 0 means "use algorithm default" (Argon2id: memory cost 19456 KB, 2 iterations).
+insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashMemoryCost', '', 0);
 
-insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashIterations', '', 2);
+insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashIterations', '', 0);
 
 insert into hm_settings (settingname, settingstring, settinginteger) values ('PasswordHashAutoUpgrade', '', 0);
 
